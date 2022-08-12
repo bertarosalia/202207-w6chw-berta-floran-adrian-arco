0b979ab285f1b1d3039da9d7252a0aee3ce0ccb8
Berta Floran <bertafloran@hotmail.com>
2022-08-12 22:39:56 +0200

    add server function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import "./loadEnvironment";
+import startServer from "./server/startServer";
 
+const port = process.env.PORT;
+
+startServer(+port);
---
+++ src/server/startServer.ts
@@ -0,0 +1,21 @@
+import "../loadEnvironment";
+import express from "express";
+import Debug from "debug";
+import chalk from "chalk";
+
+const debug = Debug("robots:startServer");
+const app = express();
+
+const startServer = (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.blue(`server listening on port http://localhost:${port}`));
+      resolve(true);
+    });
+    server.on("error", (error) => {
+      debug(chalk.red("Error connecting to database: ", error.message));
+      reject(error);
+    });
+  });
+
+export default startServer;
