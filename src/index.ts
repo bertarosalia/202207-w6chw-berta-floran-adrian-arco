7639b2150a7b21c4da1cfb0370c2e83ae8f5f0f0
Berta Floran <bertafloran@hotmail.com>
2022-08-13 14:18:46 +0200

    solve conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import "./loadEnvironment";
-import morgan from "morgan";
 import express from "express";
+import morgan from "morgan";
 import Debug from "debug";
 import chalk from "chalk";
-// import connectDatabase from "./database/connectDatabase";
 import routerRobots from "./server/routers/routersRobots";
 import { app, startServer } from "./server/startServer";
+import connectDatabase from "./database/connectDatabase";
+import getById from "./server/controllers/robotsControllers";
 
 const debug = Debug("ROBOTS:index");
 const port = process.env.PORT ?? 4500;
-// const urlMongo = process.env.MONGOURL as string;
+const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -21,5 +22,9 @@ app.use((req, rest, next) => {
 
 app.use("/robots", routerRobots);
 
+app.get("/:idRobot", getById);
+
+connectDatabase(urlMongo);
 startServer(+port);
-// connectDatabase(urlMongo);
+
+export default app;
