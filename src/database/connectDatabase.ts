712b27f3ac3b6437a3a4f96709d39bdb0eebd7e8
Berta Floran <bertafloran@hotmail.com>
2022-08-13 11:13:00 +0200

    connect to database

---
+++ src/database/connectDatabase.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import Debug from "debug";
+import chalk from "chalk";
+
+const debug = Debug("robots:database");
+
+const connectDatabase = (mongoUrl: string) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(mongoUrl, (error) => {
+      if (error) {
+        debug(chalk.red("Error conecting to database"));
+        reject(error);
+        return;
+      }
+
+      debug(chalk.green("Connected to database"));
+      resolve(true);
+    });
+  });
+
+export default connectDatabase;
--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -3,15 +3,16 @@ import express from "express";
 import Debug from "debug";
 import chalk from "chalk";
 
-const debug = Debug("robots:startServer");
+const debug = Debug("robots:server:startServer");
 const app = express();
 
 const startServer = (port: number) =>
   new Promise((resolve, reject) => {
     const server = app.listen(port, () => {
-      debug(chalk.blue(`server listening on port http://localhost:${port}`));
+      debug(`server listening on port http://localhost:${port}`);
       resolve(true);
     });
+
     server.on("error", (error) => {
       debug(chalk.red("Error connecting to database: ", error.message));
       reject(error);
